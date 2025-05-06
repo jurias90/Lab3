344320638bb731151610fc9482d860db5ea1872c
Jesus Urias <jesus.urias90@gmail.com>
2025-05-06 03:58:06 -0700

    Added a new note in main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@ int main()
     list.addCurrency(new DrachmaCurrency(15.14),0);
     list.addCurrency(new DrachmaCurrency(14.14),1);
     cout << list.stringifyList();
-    DrachmaCurrency *removed = list.removeCurrency(0);
-    removed.print();
+    /*TODO: FIgure out what the professor wants us to do with this. SInce it returns a new currency
+    Are we suppose to convert it into drachma here or somewhere in the linked list class.
+    */
+    list.removeCurrency(0);
     cout << list.stringifyList();
 }
